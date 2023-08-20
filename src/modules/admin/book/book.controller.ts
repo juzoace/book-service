871357da03b3cebe233313d4009348b43fbe43b0
juzoace <uzochukwunwigwe@gmail.com>
2023-08-20 04:15:51 +0100

    Fix admin create books swagger parameters

--- src/modules/admin/book/book.controller.ts
+++ src/modules/admin/book/book.controller.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Post, Body } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Book, CreateBooksDto } from './dto/create-book.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateBooksDto } from './dto/create-book.dto';
 import { BookService } from './book.service';
 
 @Controller('admin')
@@ -11,9 +11,9 @@ export class BookController {
 
   @Post('books')
   @ApiOperation({ summary: 'Create a new book' })
-  @ApiBody({ type: () => ({
-    books: [Book],
-  }), description: 'Array of books in the order' })
+  // @ApiBody({ type: () => ({
+  //   books: [Book],
+  // }), description: 'Array of books in the order' })
   async createBooks(@Body() data: CreateBooksDto) {
     return await this.bookService.createBooks(data);
   } 
